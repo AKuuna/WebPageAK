c67f68173ab21a2a49bf01e3d7bc0232b6a5f3cc
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-02-14 00:03:14 +0100

    adding some effects

--- behaviour.js
+++ behaviour.js
@@ -1,8 +1,13 @@
-// document.querySelectorAll('a^[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener("click", function(e){
-//         e.preventDefault();
-//         document.querySelector(this.getAttribute("href")).scrollIntoView({
-//             behavior: "smooth"
-//         });
-//     });
-// });
+document.querySelectorAll('a^[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('hfer')).scrollIntoView({
+            behavior:'smooth',
+            // alignToTop:true,
+            // block:'start'
+        });
+    });
+});
+
+// document.querySelectorAll('.anchor').scrollIntoView({ 
+//     behavior: 'smooth'}); 
