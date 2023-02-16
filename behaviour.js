e6c3f33f2c04f22debba36c848bf83b75cb20f8a
Alex Karlo <aleksandrakarlo2@gmail.com>
2023-02-17 00:11:13 +0100

    new js version on a way

--- behaviour.js
+++ behaviour.js
@@ -1,4 +1,21 @@
-document.querySelectorAll('a^[href^="#"]').forEach(anchor => {
+
+//smooth scrolling
+
+// document.querySelectorAll('.scroll').forEach(anchor => {
+//         anchor.addEventListener("click", function(e){
+//             e.preventDefault();
+//             setTimeout(() => {
+//                 console.log('Hi');
+//                 window.scrollTo({ 
+//                     top: 0, 
+//                     behavior: 'smooth'
+//                 });
+//             }, 200);
+//         });
+//     });
+
+// //otherway, works
+  document.querySelectorAll('.scroll').forEach(anchor => {
     anchor.addEventListener("click", function(e){
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
@@ -8,6 +25,13 @@ document.querySelectorAll('a^[href^="#"]').forEach(anchor => {
         });
     });
 });
+//layout
 
-// document.querySelectorAll('.anchor').scrollIntoView({ 
-//     behavior: 'smooth'}); 
+
+
+// document.addEventListener('scroll', () => {
+//     console.log(mountain1);
+//     let value = window.scrollY;
+//     mountain2.style.top = value * 1 + 'px';
+//   });
+
